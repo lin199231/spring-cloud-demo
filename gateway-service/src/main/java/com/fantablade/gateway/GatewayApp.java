2066ccf2dab28e2f15a4ada872358ecf820f624a
linfeng <lin199231@gmail.com>
2018-08-13 20:19:22 +0800

    spring cloud demo实现

--- gateway-service/src/main/java/com/fantablade/gateway/GatewayApp.java
+++ gateway-service/src/main/java/com/fantablade/gateway/GatewayApp.java
@@ -2,7 +2,9 @@ package com.fantablade.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//@EnableEurekaServer
 @SpringBootApplication
 public class GatewayApp {
     public static void main( String[] args ) {
