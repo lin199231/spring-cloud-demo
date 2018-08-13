fcb16ce167cf264939178b00e5a88b6f5743d64b
linfeng <lin199231@gmail.com>
2018-08-13 16:33:56 +0800

    删除冗余引用

--- src/test/java/com/fantablade/BaseTest.java
+++ src/test/java/com/fantablade/BaseTest.java
@@ -1,9 +1,6 @@
 package com.fantablade;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
