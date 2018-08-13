68db0e08479beb2eaea707a60ec839a1efa14862
MK <lin199231@gmail.com>
2018-08-13 23:38:30 +0800

    启动eureka服务

--- eureka-server/src/main/java/com/fantablade/eureka/EurekaServer.java
+++ eureka-server/src/main/java/com/fantablade/eureka/EurekaServer.java
@@ -2,10 +2,11 @@ package com.fantablade.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class EurekaServer {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer.class, args);
